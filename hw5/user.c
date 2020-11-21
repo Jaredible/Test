009856ec7fceb9b77e42280181d55072c3908099
jmddnb <jmddnb@umsystem.edu>
2020-11-21 16:52:30 -0600

    test

--- hw5/user.c
+++ hw5/user.c
@@ -34,8 +34,9 @@ static int msqid = -1;
 static System *system = NULL;
 static Message message;
 
-void registerSignalHandlers();
-void signalHandler(int);
+void processInterrupt();
+void processHandler(int signum);
+void resumeHandler(int signum);
 
 void initIPC();
 void crash(char*);
@@ -44,7 +45,7 @@ void init(int, char**);
 int main(int argc, char **argv) {
 	init(argc, argv);
 
-	registerSignalHandlers();
+	processInterrupt();
 
 	int i;
 	spid = atoi(argv[1]);
@@ -142,21 +143,29 @@ int main(int argc, char **argv) {
 	exit(spid);
 }
 
-void registerSignalHandlers() {
-	struct sigaction sa;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = &signalHandler;
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGALRM, &sa, NULL) == -1) crash("sigaction");
+void processInterrupt()
+{
+	struct sigaction sa1;
+	sigemptyset(&sa1.sa_mask);
+	sa1.sa_handler = &processHandler;
+	sa1.sa_flags = SA_RESTART;
+	if (sigaction(SIGUSR1, &sa1, NULL) == -1)
+	{
+		perror("ERROR");
+	}
 
-	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = &signalHandler;
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGINT, &sa, NULL) == -1) crash("sigaction");
+	struct sigaction sa2;
+	sigemptyset(&sa2.sa_mask);
+	sa2.sa_handler = &processHandler;
+	sa2.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa2, NULL) == -1)
+	{
+		perror("ERROR");
+	}
 }
-
-void signalHandler(int sig) {
-	fprintf(stderr, "p%d terminating\n", getpid());
+void processHandler(int signum)
+{
+	printf("%d: Terminated!\n", getpid());
 	exit(2);
 }
 
