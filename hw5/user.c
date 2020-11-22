407dddc50243e6c90a5870bccbbe5de911f3a4a3
jmddnb <jmddnb@umsystem.edu>
2020-11-21 20:29:20 -0600

    test

--- hw5/user.c
+++ hw5/user.c
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
 	registerSignalHandlers();
 
 	int spid = atoi(argv[1]);
-	printf("spid: %d\n", spid);
+	printf("\t\t\tspid: %d\n", spid);
 
 	srand(time(NULL) ^ getpid());
 
