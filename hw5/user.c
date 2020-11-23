629b5fd2ecb413ca2c48c6c50a15b846831f228f
jmddnb <jmddnb@umsystem.edu>
2020-11-23 16:43:09 -0600

    test

--- hw5/user.c
+++ hw5/user.c
@@ -167,12 +167,12 @@ void signalHandler(int sig) {
 void initIPC() {
 	key_t key;
 
-	if ((key = ftok(KEY_PATHNAME, KEY_ID_SYSTEM)) == -1) crash("ftok");
-	if ((shmid = shmget(key, sizeof(System), IPC_EXCL | IPC_CREAT | PERMS)) == -1) crash("shmget");
+	if ((key = ftok(".", 0)) == -1) crash("ftok");
+	if ((shmid = shmget(key, sizeof(System), 0)) == -1) crash("shmget");
 	if ((system = (System*) shmat(shmid, NULL, 0)) == (void*) -1) crash("shmat");
 
-	if ((key = ftok(KEY_PATHNAME, KEY_ID_MESSAGE_QUEUE)) == -1) crash("ftok");
-	if ((msqid = msgget(key, IPC_EXCL | IPC_CREAT | PERMS)) == -1) crash("msgget");
+	if ((key = ftok(".", 1)) == -1) crash("ftok");
+	if ((msqid = msgget(key, 0)) == -1) crash("msgget");
 }
 
 void crash(char *msg) {
